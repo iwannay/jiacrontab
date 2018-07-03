2a48407a35f4f46082cd354102607d567724456e
John Wang <john.wang@verystar.cn>
2018-07-03 12:00:18 +0800

    update

--- libs/fn.go
+++ libs/fn.go
@@ -2,6 +2,7 @@ package libs
 
 import (
 	"bufio"
+	"jiacrontab/libs/file"
 	"reflect"
 	"strconv"
 
@@ -60,12 +61,11 @@ func MRecover() {
 func SystemInfo(startTime time.Time) map[string]interface{} {
 	var afterLastGC string
 	goNum := runtime.NumGoroutine()
-	runtime.LockOSThread()
 	cpuNum := runtime.NumCPU()
 	mstat := &runtime.MemStats{}
 	runtime.ReadMemStats(mstat)
 	costTime := int(time.Since(startTime).Seconds())
-	mb := uint64(1024 * 1024)
+	mb := 1024 * 1024
 
 	if mstat.LastGC != 0 {
 		afterLastGC = fmt.Sprintf("%.1fs", float64(time.Now().UnixNano()-int64(mstat.LastGC))/1000/1000/1000)
@@ -77,12 +77,22 @@ func SystemInfo(startTime time.Time) map[string]interface{} {
 		"服务运行时间":    fmt.Sprintf("%d天%d小时%d分%d秒", costTime/(3600*24), costTime%(3600*24)/3600, costTime%3600/60, costTime%(60)),
 		"goroute数量": goNum,
 		"cpu核心数":    cpuNum,
-		"当前内存使用量":   fmt.Sprintf("%.2f MB", float64(mstat.Alloc)/float64(mb)),
-		"所有被分配的内存":  fmt.Sprintf("%.2f MB", float64(mstat.TotalAlloc)/float64(mb)),
-		"指针查找次数":    mstat.Lookups,
-		"内存分配次数":    mstat.Mallocs,
-		"内存释放次数":    mstat.Frees,
-		"距离上次GC时间":  afterLastGC,
+
+		"当前内存使用量":  file.FileSize(int64(mstat.Alloc)),
+		"所有被分配的内存": file.FileSize(int64(mstat.TotalAlloc)),
+		"内存占用量":    file.FileSize(int64(mstat.Sys)),
+		"指针查找次数":   mstat.Lookups,
+		"内存分配次数":   mstat.Mallocs,
+		"内存释放次数":   mstat.Frees,
+		"距离上次GC时间": afterLastGC,
+
+		"当前 Heap 内存使用量": file.FileSize(int64(mstat.HeapAlloc)),
+		"Heap 内存占用量":    file.FileSize(int64(mstat.HeapSys)),
+		"Heap 内存空闲量":    file.FileSize(int64(mstat.HeapIdle)),
+		"正在使用的 Heap 内存": file.FileSize(int64(mstat.HeapInuse)),
+		"被释放的 Heap 内存":  file.FileSize(int64(mstat.HeapReleased)),
+		"Heap 对象数量":     mstat.HeapObjects,
+
 		"下次GC内存回收量": fmt.Sprintf("%.3fMB", float64(mstat.NextGC)/float64(mb)),
 		"GC暂停时间总量":  fmt.Sprintf("%.3fs", float64(mstat.PauseTotalNs)/1000/1000/1000),
 		"上次GC暂停时间":  fmt.Sprintf("%.3fs", float64(mstat.PauseNs[(mstat.NumGC+255)%256])/1000/1000/1000),
