6ab9b0c69b3ea05f2ded882d5ea1cbf45bd54a65
iwannay <772648576@qq.com>
2018-10-20 13:51:47 +0800

    update 倒序读取

--- libs/finder/reader.go
+++ libs/finder/reader.go
@@ -41,7 +41,7 @@ func (t *TailReader) Read(b []byte) (n int, err error) {
 		return n, err
 	}
 
-	invertSplit(b, n)
+	invert(b[0:n])
 
 	if off == 0 {
 		t.isEOF = true
@@ -61,32 +61,4 @@ func invert(b []byte) {
 	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
 	}
-}
-
-func invertSplit(b []byte, n int) {
-	invert(b[0:n])
-	var k, i int
-
-	for k, i = 0, 0; k < n; k++ {
-		if b[k] == '\n' && n > k+1 && b[k+1] == '\r' {
-			b[k], b[k+1] = '\r', '\n'
-			k++
-			if i != k {
-				invert(b[i:k])
-			}
-
-			i = k + 1
-			continue
-		} else if b[k] == '\n' {
-			if i != k {
-				invert(b[i:k])
-			}
-			i = k + 1
-		}
-	}
-
-	if i != k {
-		invert(b[i:k])
-	}
-
 }
