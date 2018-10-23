f76edf5c5a2d49ddbe365e45360832d6a0a467bb
iwannay <772648576@qq.com>
2018-10-23 11:01:20 +0800

    update

--- server/handle/crontab_task.go
+++ server/handle/crontab_task.go
@@ -508,11 +508,7 @@ func RecentLog(ctx iris.Context) {
 		Pattern:  pattern,
 		IsTail:   isTail,
 	}, &searchRet); err != nil {
-
 		ctx.ViewData("error", err)
-		ctx.View("public/error.html")
-		return
-
 	}
 	logList := strings.Split(string(searchRet.Content), "\n")
 
