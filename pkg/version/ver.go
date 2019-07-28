f7b3b332c1263438a0f1db268ac50702b19bf47f
iwannay <772648576@qq.com>
2019-07-28 22:03:57 +0800

    2.0.0-alpha726

--- pkg/version/ver.go
+++ pkg/version/ver.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const Binary = "2.0.0-alpha721"
+const Binary = "2.0.0-alpha726"
 
 func String(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, Binary, runtime.Version())
