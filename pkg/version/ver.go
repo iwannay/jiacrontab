1a88f182122742228eb8faa6589bb5c0f4c31f41
iwannay <772648576@qq.com>
2019-08-07 21:31:06 +0800

    update v2.0.3

--- pkg/version/ver.go
+++ pkg/version/ver.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const Binary = "2.0.2"
+const Binary = "2.0.3"
 
 func String(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, Binary, runtime.Version())
