92815e46420d21464f472247d13aa10004864db4
matthew188 <matthewyuan18@foxmail.com>
2020-03-12 11:44:56 +0800

    fix：替换数据库产生的unknown driver err

--- app/jiacrontabd/main.go
+++ app/jiacrontabd/main.go
@@ -11,6 +11,8 @@ import (
 	"os"
 
 	"github.com/iwannay/log"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
